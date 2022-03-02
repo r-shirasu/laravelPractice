35857f6c679aef21e8555ad52d92f867c7be3543
r-shirasu <risako.shirasu@asia-quest.jp>
2022-03-02 23:32:54 +0900

    until#10

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<?php
+
+// var_dump($posts);
+// exit;
+//dd($posts);
+
+?>
+    <!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+    <h1>My BBS</h1>
+    <ul>
+        <li>Title</li>
+        <li>Title</li>
+        <li>Title</li>
+    </ul>
+</div>
+</body>
+</html>
