b392044120661e9ceee2bd0e017f1f3d1b83cec0
r-shirasu <risako.shirasu@asia-quest.jp>
2022-03-02 23:38:51 +0900

    #11:use blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,9 +16,10 @@
 <div class="container">
     <h1>My BBS</h1>
     <ul>
-        <li>Title</li>
-        <li>Title</li>
-        <li>Title</li>
+        {{-- <li><?php echo htmlspecialchars($posts[0], ENT_QUOTES, 'UTF-8'); ?></li> --}}
+        <li>{{ $posts[0] }}</li>
+        <li>{{ $posts[1] }}</li>
+        <li>{{ $posts[2] }}</li>
     </ul>
 </div>
 </body>
